5d53ef0285ec2e4093c78778ccafcb50bf304588
Sharon Watkins <swatkins876@gmail.com>
2019-07-28 13:26:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,9 @@
 
 first_number = 12
-    second_number = 11
-    difference = (first_number+second_number)
+second_number = 11
+difference = (first_number+second_number)
+    
+first_number = 12
+second_number = 11
+sum = (first_number+second_number)
+    
