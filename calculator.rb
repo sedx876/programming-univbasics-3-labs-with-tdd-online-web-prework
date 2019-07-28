7204243fbc06caf7e52c5817b021d266e3770208
Sharon Watkins <swatkins876@gmail.com>
2019-07-28 13:30:37 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,18 @@
-# Add your variables here
+
+first_number = 12
+second_number = 11
+difference = (first_number-second_number)
+    
+first_number = 12
+second_number = 11
+sum = (first_number+second_number)
+    
+first_number = 12
+second_number = 11
+product = (first_number*second_number)
+
+first_number = 12
+second_number = 11
+quotient = (first_number/second_number)
+    
+        
