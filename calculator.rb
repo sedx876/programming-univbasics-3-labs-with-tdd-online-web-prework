21c6de53993aea3534aaab63383a012d3d41bcb9
Sharon Watkins <swatkins876@gmail.com>
2019-07-28 13:28:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -10,4 +10,9 @@ sum = (first_number+second_number)
 first_number = 12
 second_number = 11
 product = (first_number*second_number)
+
+first_number = 12
+second_number = 11
+quotient = (first_number/second_number)
+    
         
